7e665291b4409fc0bb16066e8c7047caf8aea69a
giuseppe <placidagiuseppe@gmail.com>
2021-03-14 23:33:03 +0100

    completo

--- main.js
+++ main.js
@@ -1,61 +1,77 @@
 //utilizzo la funzione .ready per essere sicuro di far eseguire il codice dopo il caricamento del DOM
 $(document).ready(function(){
   carosel();
+  generatepallini()
 })
 
 function carosel(){
   var frecciaSx = $('.prev > i.fa-angle-left');
   var frecciaDx = $('.next > i.fa-angle-right');
+  var pallini = $('.nav i');
 
-   var pallini = $('.nav i');
-   var figura = $('.images img');
-   for (var i = 0; i < figura.length; i++) {
-        figura[i].setAttribute('id',i);
-        pallini[i].setAttribute('id',i);
-   }
 
   pallini.click(function(){
-    var id = this.getAttribute('id');
-    var imagine = $('img#' + id);
-    var imaginevecchia=$('img.active');
+    var dotsposition = pallini.index(this);
+    var images = $('.images img');
+    pallini.removeClass('active');
+    $(this).addClass('active');
+    images.removeClass('active');
+    images.eq(dotsposition).addClass('active');
 
-    var pallino = pallini[id];
-    var pallinovecchio = $('i.active');
-    pallinovecchio.removeClass('active');
+  })
 
-    $(pallino).addClass('active');
+  frecciaDx.click(function(e){
+    next();
+  })
 
-    switch (imagine.hasClass()) {
-      case !'active':
+  frecciaSx.click(function(e){
+    prev();
+  })
 
-        imaginevecchia.removeClass('active');
-        imagine.addClass('active');
-        break;
+  $(document).Keyup(function (e){
+    if(e.KeyCode == "39"){
+      next();
     }
-
+    if (e.KeyCode == "37") {
+      prev();
+    }
   })
 
 
-  frecciaDx.click(function(){
-      var img = $('.active');
-      var first = $(' .first');
-      var cerchio =$('.nav i.active');
-      var cerchiofirst = $('.nav .first');
-      img.removeClass('active').next().addClass('active');
+}
+function next(){
+  var img = $('.active');
+  var first = $(' .first');
+  var cerchio =$('.nav i.active');
+  var cerchiofirst = $('.nav .first');
+  img.removeClass('active').next().addClass('active');
 
-      if (img.hasClass('last')) {
-        img.removeClass('active');
-        first.addClass('active');
-      }
-  })
+  if (img.hasClass('last')) {
+    img.removeClass('active');
+    first.addClass('active');
+  }
+}
+function prev(){
+  var img = $('.active');
+  var last = $('.last');
+  img.removeClass('active').prev().addClass('active');
+   if (img.hasClass('first')) {
+     img.removeClass('active');
+     last.addClass('active');
+   }
+}
+function generatepallini(){
+  var nav = $('.slider-wrapper .nav');
+  var raccolta = $('.slider-wrapper .iamges img').length;
 
-  frecciaSx.click(function(){
-    var img = $('.active');
-    var last = $('.last');
-    img.removeClass('active').prev().addClass('active');
-     if (img.hasClass('first')) {
-       img.removeClass('active');
-       last.addClass('active');
-     }
-  })
+  for (var i = 0; i < raccolta.length; i++) {
+    if (i==0) {
+    var dom =nav.html()+'<i class="fas fa-circle active first"></i>';
+    }else if (i==raccolta-1) {
+    var dom =nav.html()+'<i class="fas fa-circle active last"></i>';
+    }else {
+    var dom =nav.html()+'<i class="fas fa-circle"></i>';
+    }
+    nav.html(dom);
+  }
 }
